5bb06f78e4983242f52d0043155b33bbe0a05be1
sinetek <pitwuu@gmail.com>
2021-09-21 12:00:59 +0200

    TODO FetchWalletTLTransactions.

--- src/tradelayer/fetchwallettx.cpp
+++ src/tradelayer/fetchwallettx.cpp
@@ -53,71 +53,7 @@ unsigned int GetTransactionByteOffset(const uint256& txid)
  */
 std::map<std::string, uint256> FetchWalletTLTransactions(unsigned int count, int startBlock, int endBlock)
 {
-    std::map<std::string, uint256> mapResponse;
-#ifdef ENABLE_WALLET
-    CWalletRef pwalletMain = nullptr;
-    if (vpwallets.size() > 0){
-        pwalletMain = vpwallets[0];
-    }
-
-    if (pwalletMain == nullptr) {
-        return mapResponse;
-    }
-
-    std::set<uint256> seenHashes;
-    CWallet::TxItems txOrdered;
-    {
-        LOCK(pwalletMain->cs_wallet);
-        txOrdered = pwalletMain->wtxOrdered;
-    }
-    
-    // Iterate backwards through wallet transactions until we have count items to return:
-    for (CWallet::TxItems::reverse_iterator it = txOrdered.rbegin(); it != txOrdered.rend(); ++it)
-    {
-        const CWalletTx* pwtx = it->second.first;
-        if (pwtx == nullptr) continue;
-        const uint256& txHash = pwtx->GetHash();
-        {
-            LOCK(cs_tally);
-            if (!p_txlistdb->exists(txHash)) continue;
-        }
-
-        const uint256& blockHash = pwtx->hashBlock;
-        if (blockHash.IsNull() || (nullptr == GetBlockIndex(blockHash))) continue;
-        const CBlockIndex* pBlockIndex = GetBlockIndex(blockHash);
-        if (nullptr == pBlockIndex) continue;
-        int blockHeight = pBlockIndex->nHeight;
-        if (blockHeight < startBlock || blockHeight > endBlock) continue;
-        int blockPosition = GetTransactionByteOffset(txHash);
-        std::string sortKey = strprintf("%07d%010d", blockHeight, blockPosition);
-        mapResponse.insert(std::make_pair(sortKey, txHash));
-        seenHashes.insert(txHash);
-        if (mapResponse.size() >= count) break;
-    }
-
-    // Insert pending transactions (sets block as 999999 and position as wallet position)
-    // TODO: resolve potential deadlock caused by cs_wallet, cs_pending
-    // LOCK(cs_pending);
-    for (PendingMap::const_iterator it = my_pending.begin(); it != my_pending.end(); ++it)
-    {
-        const uint256& txHash = it->first;
-        int blockHeight = 9999999;
-        if (blockHeight < startBlock || blockHeight > endBlock) continue;
-        int blockPosition = 0;
-        {
-            LOCK(pwalletMain->cs_wallet);
-            std::map<uint256, CWalletTx>::const_iterator walletIt = pwalletMain->mapWallet.find(txHash);
-            if (walletIt != pwalletMain->mapWallet.end()) {
-                const CWalletTx& wtx = walletIt->second;
-                blockPosition = wtx.nOrderPos;
-            }
-        }
-
-        std::string sortKey = strprintf("%07d%010d", blockHeight, blockPosition);
-        mapResponse.insert(std::make_pair(sortKey, txHash));
-    }
-#endif
-    return mapResponse;
+#warning xxx
 }
 
 
